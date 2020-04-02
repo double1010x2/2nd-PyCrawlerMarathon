6a7c74cae1f066275dfeaad23add574023261fb6
double1010x2 <double1010x2@gmail.com>
2020-04-03 02:26:06 +0800

    [Day-007] homework: Day007_HW.py note:     learn http and javascript

---
+++ homework/Day007_HW.py
@@ -0,0 +1,58 @@
+# %%
+'''
+# HTTP 靜態網頁架構說明
+
+* 認識靜態網頁的溝通架構與運作原理
+* HTML、CSS、JavaScript 在網頁中扮演的角色
+* 網頁中定位資料的方法
+'''
+
+# %%
+'''
+## 作業目標
+
+1. （簡答題）請問目前提到的這種基於 HTTP 一來一回的網頁傳輸方式，有哪些缺點及問題？
+2. （簡答題）網頁中使用的 JavaScipt 程式語言，跟我們一般使用的 Python 在執行上有什麼不同。
+
+'''
+
+# %%
+'''
+### 1.（簡答題）請問目前提到的這種基於 HTTP 一來一回的網頁傳輸方式，有哪些缺點及問題？
+'''
+
+# %%
+'''
+Reference: https://www.itread01.com/p/1365273.html
+1. slow and high connection loading
+2. large loading when dynamic interaction between client and server
+3. low security
+'''
+
+# %%
+'''
+### 2. （簡答題）網頁中使用的 JavaScipt 程式語言，跟我們一般使用的 Python 在執行上有什麼不同。
+'''
+# %%
+'''
+Reference:
+    https://www.guru99.com/python-vs-javascript.html
+
+1. JavaScript has no concept of mutable and immutable, but python has mutable and immutable data types.
+2. JavaScript should be encoded as UTF-16 as it does not offer any built-in support for manipulating raw bytes.	Python source code is ASCII by default unless you are specifying any encoding format.
+3. JavaScript has only floating-point numbers.	In Python, there are many different numeric types like int, fixed-point decimal, etc.
+4. JavaScript has fewer modules like date, math, regexp, JSON.	Python includes a wide range of modules.
+5. JavaScript helps you to build a website or native application.	Python is for tasks related to data analytics, machine learning, and math-intensive operations.
+6. JavaScript objects have properties that can be composed of underlying attributes which let you define a property.	In Python programming language, getter and setter functions are used to defining an attribute.
+7. avaScript uses a prototype-based inheritance model.	Python uses the class-based inheritance model.
+
+[Disadvantages of JavaScript]
+(1)Client-side JavaScript does not allow the reading or writing of files. It has been kept for security reasons.
+(2)JavaScript can't be used for networking applications because there is not much support available.
+(3)JavaScript doesn't have any multiprocessor multithreading features.
+
+[Disadvantages of Python]
+As Python is dynamic, so it shows more errors at run-time.
+'''
+
+# %%
